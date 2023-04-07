d7a18766cbe879d79f9b7e16fbafd9e80b1ac714
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-04-07 13:51:30 +0200

    Add mobile version (without navigation)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,27 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
-      fontFamily: {},
-      colors: {},
+      fontFamily: {
+        body: ['Inter', 'san-serif'],
+      },
+      fontSize: {
+        base: [
+          '15px',
+          {
+            letterSpacing: '0.02em',
+            fontWeight: '400',
+            lineHeight: '1.5rem',
+          },
+        ],
+      },
+      colors: {
+        'soft-orange': 'hsl(35, 77%, 62%)',
+        'soft-red': 'hsl(5, 85%, 63%)',
+        'off-white': 'hsl(36, 100%, 99%)',
+        'grayish-blue': 'hsl(233, 8%, 79%)',
+        'dark-grayish-blue': 'hsl(236, 13%, 42%)',
+        'very-dark-blue': 'hsl(240, 100%, 5%)',
+      },
     },
   },
   plugins: [],
